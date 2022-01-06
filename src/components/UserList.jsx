e008e7ee1734889b8abe9ace4d810d7faf38703d
Didier Dakoury <dazibao76@gmail.com>
2022-01-06 13:17:15 +0000

    third commit

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -13,13 +13,23 @@ const UserList = ({listOfUser}) => {
                         <br />
                         <span><b>Email</b><br /> {user.email} </span>
                         <br />
-                        {/* {user.address}  */}
+                        <span><b>street</b><br /> {user.address.street} </span>
+                        <br />
+                        <span><b>suite</b><br /> {user.address.suite} </span>
+                        <br />
+                        <span><b>city</b><br /> {user.address.city} </span>
+                        <br />
+                        <span><b>zipcode</b><br /> {user.address.zipcode} </span>
                         <br />
                         <span><b>Phone</b><br /> {user.phone} </span>
                         <br />
                         <span><b>Website</b><br /> {user.website} </span>
                         <br />
-                        {/* {user.company} */}
+                        <span><b>Company name</b><br /> {user.company.name} </span>
+                        <br />
+                        <span><b>catchPhrase</b><br /> {user.company.catchPhrase} </span>
+                        <br />
+                        <span><b>bs</b><br /> {user.company.bs} </span>
                     </li>)}
             </ul>
         </div>
